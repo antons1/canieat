18eeaecf5509e610d07f29caec039724c6250493
Håkon Antonsen <haakon@antons1.net>
2016-09-17 01:09:54 +0200

    Hardcoded food for prototyping

---
+++ frontend/hardcodedfood.js
@@ -0,0 +1,29 @@
+module.exports = [
+    {
+        "name": "Melk",
+        "amount": 3,
+        "baseline": 3,
+        "unit": {
+            "singular": "liter",
+            "plural": "liter"
+        }
+    },
+    {
+        "name": "Ost",
+        "amount": 1,
+        "baseline": 1,
+        "unit": {
+            "singular": "pakke",
+            "plural": "pakker"
+        }
+    },
+    {
+        "name": "Agurk",
+        "amount": 1,
+        "baseline": 2,
+        "unit": {
+            "singular": "stk",
+            "plural": "stk"
+        }
+    }
+];
