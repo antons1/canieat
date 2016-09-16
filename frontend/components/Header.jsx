0355052735c2e70da12d2aabbb258011f6fe8174
Håkon Antonsen <haakon@antons1.net>
2016-09-17 01:10:21 +0200

    Simple header

---
+++ frontend/components/Header.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const Header = (props) =>
+    <header className="top-bar main-header">
+        <div className="top-bar-left">
+        </div>
+        <div className="top-bar-right">
+
+        </div>
+        <div className="row"><h1>Can I eat?</h1></div>
+    </header>;
+
+module.exports = Header;
